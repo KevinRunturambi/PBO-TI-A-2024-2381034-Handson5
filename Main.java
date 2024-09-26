f403df97c26741e2e0b228d09cc6ccbedb4ccefd
KevinRunturambi <2381034@unai.edu>
2024-09-26 10:11:00 +0700

    Handson5

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner
+
+public class Main {
+    public static String[] todos = new String[10];
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        addTodolist("Membaca");
+        addTodoList("Makan");
+        showTableTodo();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Todo List");
+        for (int i = 0; i < todos.length; i++) {
+            String todo = todos[i];
+            if (todos[i])
+    }
+        todos[0] = "memba"
+    }
+}
